c73dabb538db631dcf7f508344d6797ece4907a7
Jennifer Stoos <jstoos@bitlaw.com>
2018-03-21 02:03:16 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,7 +33,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT title, SUM(characters.species)
+  "SELECT title
   FROM series
   LEFT JOIN characters
   ON characters.series_id = series.id
