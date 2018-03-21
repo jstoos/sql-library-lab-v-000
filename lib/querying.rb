d8a783521adae0a7a2fecb32dc161e63663a048e
Jennifer Stoos <jstoos@bitlaw.com>
2018-03-21 02:07:11 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -48,6 +48,6 @@ def select_character_names_and_number_of_books_they_are_in
   ON character_books.character_id = character.id
   INNER JOIN books
   ON character_books.book_id = books.id
-  
+  LIMIT 1
   "
 end
