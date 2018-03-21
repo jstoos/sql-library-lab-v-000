d35db840a73ad1b5a6854d911e538a3a8c9282a9
Jennifer Stoos <jstoos@bitlaw.com>
2018-03-21 01:58:51 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,10 +23,12 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT name, series.title
+  "SELECT name, subgenres.name
   FROM authors
   LEFT JOIN series
   ON authors.id = series.author_id
+  INNER JOIN subgenres
+  ON subgenres.id = series.subgenre.id
   "
 end
 
